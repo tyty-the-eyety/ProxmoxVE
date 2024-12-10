72cf5463bfcfb8b6ebfd77c3f99eab2ae58a6ddd
tyty-the-eyety <tyronesch@gmail.com>
2024-12-10 19:17:24 +0000

    testing sonarqube install

---
+++ ct/sonarqube.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tyty-the-eyety/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+   _____                         ____        __          
+  / ___/____  ____  ____ ______/ __ \__  __/ /_  ___   
+  \__ \/ __ \/ __ \/ __ `/ ___/ / / / / / / __ \/ _ \  
+ ___/ / /_/ / / / / /_/ / /  / /_/ / /_/ / /_/ /  __/  
+/____/\____/_/ /_/\__,_/_/   \___\_\__,_/_.___/\___/   
+                                                      
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="SonarQube"
+var_disk="8"
+var_cpu="2"
+var_ram="4096"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /opt/sonarqube ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+
+msg_info "Stopping ${APP}"
+systemctl stop sonarqube
+msg_ok "Stopped ${APP}"
+
+CURRENT_VERSION=$(cat /opt/${APP}_version.txt)
+LATEST_VERSION=$(curl -s https://api.github.com/repos/SonarSource/sonarqube/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+
+if [[ "${LATEST_VERSION}" != "${CURRENT_VERSION}" ]]; then
+  msg_info "Updating ${APP} to ${LATEST_VERSION}"
+  
+  msg_info "Backing up configuration"
+  cp -R /opt/sonarqube/conf /root/sonarqube_conf_backup
+  msg_ok "Backed up configuration"
+  
+  cd /opt
+  wget -q https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${LATEST_VERSION#*v}.zip
+  unzip -q sonarqube-${LATEST_VERSION#*v}.zip
+  rm sonarqube-${LATEST_VERSION#*v}.zip
+  rm -rf /opt/sonarqube
+  mv sonarqube-${LATEST_VERSION#*v} sonarqube
+  
+  msg_info "Restoring configuration"
+  cp -R /root/sonarqube_conf_backup/* /opt/sonarqube/conf/
+  rm -rf /root/sonarqube_conf_backup
+  msg_ok "Restored configuration"
+  
+  chown -R sonarqube:sonarqube /opt/sonarqube
+  echo "${LATEST_VERSION}" > /opt/${APP}_version.txt
+  msg_ok "Updated ${APP} to ${LATEST_VERSION}"
+  
+  msg_info "Starting ${APP}"
+  systemctl start sonarqube
+  msg_ok "Started ${APP}"
+  msg_ok "Update Completed Successfully!"
+else
+  msg_ok "No update required. ${APP} is already at ${LATEST_VERSION}"
+fi
+}
+
+start
+build_container
+description
---
+++ install/sonarqube-install.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y unzip
+$STD apt-get install -y postgresql
+$STD apt-get install -y postgresql-contrib
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Java 17"
+$STD apt-get install -y openjdk-17-jdk
+msg_ok "Installed Java 17"
+
+msg_info "Setting up PostgreSQL"
+$STD systemctl start postgresql
+$STD su - postgres -c "createuser sonarqube"
+$STD su - postgres -c "createdb -O sonarqube sonarqube"
+$STD su - postgres -c "psql -c \"ALTER USER sonarqube WITH ENCRYPTED password 'sonarqube';\""
+msg_ok "Set up PostgreSQL"
+
+msg_info "Installing SonarQube"
+RELEASE=$(curl -s https://api.github.com/repos/SonarSource/sonarqube/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+VERSION=${RELEASE#*v}
+
+cd /opt
+wget -q https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${VERSION}.zip
+unzip -q sonarqube-${VERSION}.zip
+mv sonarqube-${VERSION} sonarqube
+rm sonarqube-${VERSION}.zip
+
+# Create sonarqube user
+useradd -r -M -d /opt/sonarqube -s /sbin/nologin sonarqube
+chown -R sonarqube:sonarqube /opt/sonarqube
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
+
+msg_info "Configuring System Limits"
+cat <<EOF >>/etc/sysctl.conf
+vm.max_map_count=262144
+fs.file-max=65536
+EOF
+$STD sysctl -p
+
+cat <<EOF >>/etc/security/limits.conf
+sonarqube   -   nofile   65536
+sonarqube   -   nproc    4096
+EOF
+
+msg_info "Configuring SonarQube"
+cat <<EOF >/opt/sonarqube/conf/sonar.properties
+sonar.jdbc.username=sonarqube
+sonar.jdbc.password=sonarqube
+sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube
+sonar.web.javaAdditionalOpts=-server
+sonar.search.javaOpts=-Xmx512m -Xms512m -XX:MaxDirectMemorySize=256m -XX:+HeapDumpOnOutOfMemoryError
+EOF
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/sonarqube.service
+[Unit]
+Description=SonarQube service
+After=syslog.target network.target postgresql.service
+
+[Service]
+Type=forking
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+User=sonarqube
+Group=sonarqube
+Restart=always
+LimitNOFILE=65536
+LimitNPROC=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now sonarqube
+msg_ok "Created Service"
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
